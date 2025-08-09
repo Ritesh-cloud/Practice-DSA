03fd2b22bc226e585ededce26fcc3f1df4761940
Ritesh Murmu <126056160+Ritesh-cloud@users.noreply.github.com>
2025-08-09 19:56:48 +0530

    Add files via upload
    git commit -m "Fix logic for 'Concatenation of Array' problem: Remove redundant loop and hardcoded values"

---
+++ concatenations.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getConcatenation(self, nums: list[int]) -> list[int]:
+        ans = [22,21,20,1]
+        for i in range(1):
+            for n in nums:
+                ans.append(n)
+        return ans   
+
+
+class solution:
+    def getconcatenation(self, nums:list[int]) ->list[int]:
+        ans = [1,4,1,2]
+        for i in range(1):
+            for n in nums:
+                ans.append(n)
+        return ans        
